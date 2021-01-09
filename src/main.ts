fc8d1bb842ac425357662f3aebaabeab696a8ac2
Sesh Sadasivam <seshrs@umich.edu>
2021-01-08 19:25:19 -0500

    Update inputs, add GitHub context

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,21 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from "@actions/core";
+import github from "@actions/github";
+import * as Webhooks from "@octokit/webhooks";
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    // const githubToken = core.getInput("GH_TOKEN");
+    // const octokit = github.getOctokit(githubToken);
+    const event = github.context
+      .payload as Webhooks.EventPayloads.WebhookPayloadPullRequest;
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const repo = `${github.context.repo.owner}/${github.context.repo.repo}`;
+    const prNumber = event.number;
 
-    core.setOutput('time', new Date().toTimeString())
+    core.info(`PR #${prNumber} on repo ${repo}`);
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed(error.message);
   }
 }
 
-run()
+run();
