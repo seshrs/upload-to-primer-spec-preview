d8273409c6613616eb925f245c8e114f537d725b
Sesh Sadasivam <seshrs@umich.edu>
2021-01-14 10:23:16 -0500

    Add expiry date to comment Also update comment with new expiry date on each rerun

--- src/createOrUpdateComment.ts
+++ src/createOrUpdateComment.ts
@@ -17,28 +17,48 @@ export default async function createOrUpdateComment(
   repo: RepoType,
   prNumber: number
 ): Promise<void> {
-  if (await findPreviousComment(octokit, repo, prNumber)) {
-    core.info("Skipping since previous comment exists.");
-    return;
-  }
+  const commentBody = `${HEADER_COMMENT}\nThe spec from this PR is available at https://preview.seshrs.ml/previews/${
+    repo.owner
+  }/${
+    repo.repo
+  }/${prNumber}/.\n\n<sup>(Available until ${getExpiryDate()}.)</sup>`;
+  core.info(`Using comment body:\n${commentBody}`);
 
-  const commentBody = `${HEADER_COMMENT}\nThe spec from this PR is available at https://preview.seshrs.ml/previews/${repo.owner}/${repo.repo}/${prNumber}/.`;
-  await octokit.issues.createComment({
-    ...repo,
-    issue_number: prNumber,
-    body: commentBody,
-  });
-  core.info(commentBody);
+  const previousCommentId = await findPreviousComment(octokit, repo, prNumber);
+  if (previousCommentId) {
+    core.info("Updating previous comment.");
+    await octokit.issues.updateComment({
+      ...repo,
+      comment_id: previousCommentId,
+      body: commentBody,
+    });
+  } else {
+    core.info("Creating new comment.");
+    await octokit.issues.createComment({
+      ...repo,
+      issue_number: prNumber,
+      body: commentBody,
+    });
+  }
 }
 
 async function findPreviousComment(
   octokit: Octokit,
   repo: RepoType,
   issue_number: number
-): Promise<boolean> {
+): Promise<number | null> {
   const { data: comments } = await octokit.issues.listComments({
     ...repo,
     issue_number,
   });
-  return !!comments.find((comment) => comment.body?.includes(HEADER_COMMENT));
+  return (
+    comments.find((comment) => comment.body?.includes(HEADER_COMMENT))?.id ??
+    null
+  );
+}
+
+function getExpiryDate(): string {
+  const date = new Date();
+  date.setDate(date.getDate() + 30);
+  return date.toDateString();
 }
